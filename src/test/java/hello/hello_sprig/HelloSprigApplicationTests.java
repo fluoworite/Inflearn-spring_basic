56010409dc9c937cbdb21cfb0344233bfbe51a07
Hyeongseok <a1932a@naver.com>
2024-07-19 05:58:04 +0900

    API

---
+++ src/test/java/hello/hello_sprig/HelloSprigApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hello_sprig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSprigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
