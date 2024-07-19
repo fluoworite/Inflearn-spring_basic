8982b12dcc8b37e916fc801769efd91d8eaa737e
Hyeongseok <a1932a@naver.com>
2024-07-19 16:00:32 +0900

    회원 서비스 테스트

--- src/main/java/hello/hello_sprig/service/MemberService.java
+++ src/main/java/hello/hello_sprig/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     public Long join(Member member) {
         //같은 이름이 있는 중복 회원X
@@ -32,7 +36,7 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findMember(Long id) {
+    public Optional<Member> findOne(Long id) {
         return memberRepository.findbyId(id);
     }
 }
